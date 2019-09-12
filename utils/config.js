9e8f9c99bfb38c464cd5db06981bc028d6f14254
18067088102 <15324443802@163.com>
2019-09-12 08:43:46 +0800

    2019-9-12

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const config = {//www.ysa.cncqs.cn:8000   192.168.0.51   192.168.0.135:8081  https://fd.yishian.net:8443/applet/
-  api_base_url: 'https://192.168.0.51/applet/',
+  api_base_url: 'https://www.ysa.cncqs.cn:8000/applet/',
   appkey: "8IJW8QSdFBJPsTOI",
 }
 
